80f18837475be51c18e5f800d0a642738beead3d
Sergio Tapia-Fikes <stapiafikes@gmail.com>
2019-07-12 21:43:08 -0500

    nobull product landing page first commit

---
+++ app_ideas.js
@@ -0,0 +1,23 @@
+/* Have idea for drop down menu for Best-sellers, high-top, 
+designer and then click on menu item to direct you to place on page 
+that has section for each item. perhaps on click make image and video size bigger to 
+show which item is selected. 
+
+2) on hover play video links 
+    May be a good idea to store the video links as an array
+     and access them via index? 
+
+*/ 
+
+/*let workingHard_span = document.getElementById('working-hard-message').innerHTML;
+let blackHeather = document.getElementById('black-heather');
+
+blackHeather.addEventListener("mouseover", function() {
+   let video1 =  document.getElementById("bh-video"); 
+   video1.classList.add('visible');
+}); 
+blackHeather.addEventListener("mouseout", function() {
+    let video1= document.getElementById("bh-video"); 
+    video1.classList.remove('visible');
+})
+*/
