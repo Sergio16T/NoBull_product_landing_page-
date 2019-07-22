af911e290449314759ffe252b7a273f9f7ad6e48
Sergio Tapia-Fikes <stapiafikes@gmail.com>
2019-07-21 19:05:26 -0500

    JS edits applied

--- app.js
+++ app.js
@@ -6,27 +6,25 @@ let dropDownContent_div = document.querySelector('dropdown-content')
 
 
 
-/* idea 1 create an array to store all of the href links*/ 
+/* idea 1 create an array to store all of the href links 
 const dropDown = [blackHeather_div, heatherForest_div]; 
+instead create a node list with querySelectorAll
+*/
+const dropDown = document.querySelectorAll('.shoeimage');
 
 blackHeatherDD_div.addEventListener("click", function (e) {
-    dropDown.forEach(x => x.classList.remove('shadow'));
+    dropDown.forEach(shoeimage => shoeimage.classList.remove('shadow'));
     blackHeather_div.classList.add('shadow');
     blackHeather_div.scrollIntoViewIfNeeded();
 });
+// dropDown.forEach(x => x.classList.remove('shadow')); 
+// can be written as x for html element
 
 heatherForestDD_div.addEventListener("click", function (e) {
-    dropDown.forEach(x => x.classList.remove('shadow'));
+    dropDown.forEach(shoeimage=> shoeimage.classList.remove('shadow'));
     heatherForest_div.classList.add('shadow'); 
     heatherForest_div.scrollIntoViewIfNeeded();
 });
 
- // idea 2 stack overflow, 
-/*
- window.onclick = function (e) {
-    if (e.target.localName == 'a') {
-        console.log('a tag clicked!');
-    }
-}
-*/ 
+
 
