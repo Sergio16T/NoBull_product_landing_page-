3b9219b742c989ed4bbb1ba10ae40d0cd7729d84
Sergio Tapia-Fikes <stapiafikes@gmail.com>
2019-08-01 22:39:32 -0500

    edits made 8/1

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-let blackHeather_div = document.getElementById('black-heather'); 
-let blackHeatherDD_div = document.getElementById('Black-Heather-DD'); 
-let heatherForest_div =document.getElementById('white-heather-forest'); 
-let heatherForestDD_div = document.getElementById('White-Heather-DD')
+const blackHeather_div = document.getElementById('black-heather'); 
+const blackHeatherDD_div = document.getElementById('Black-Heather-DD'); 
+const heatherForest_div =document.getElementById('white-heather-forest'); 
+const heatherForestDD_div = document.getElementById('White-Heather-DD');
+const cabernetHighTop_div = document.getElementById('cabernet-hightop');
+const cabernetHighTopDD_div = document.getElementById('Cabernet-DD');
+const springMoss_div = document.getElementById('spring-moss');
+const springMossDD_div = document.getElementById('Spring-Moss-DD');
+const woodStock_div = document.getElementById('woodstock'); 
+const woodStockDD_div = document.getElementById('WoodStock-DD'); 
+
+
 let dropDownContent_div = document.querySelector('dropdown-content')
 
 
@@ -12,19 +20,38 @@ instead create a node list with querySelectorAll
 */
 const dropDown = document.querySelectorAll('.shoeimage');
 
+// dropDown.forEach(x => x.classList.remove('shadow')); 
+// can be written as x for html element
+
+// must be a better way to write below code can be written as 1 function with a this...
+// I'm sure of it. using "this"
 blackHeatherDD_div.addEventListener("click", function (e) {
     dropDown.forEach(shoeimage => shoeimage.classList.remove('shadow'));
     blackHeather_div.classList.add('shadow');
     blackHeather_div.scrollIntoViewIfNeeded();
 });
-// dropDown.forEach(x => x.classList.remove('shadow')); 
-// can be written as x for html element
 
 heatherForestDD_div.addEventListener("click", function (e) {
     dropDown.forEach(shoeimage=> shoeimage.classList.remove('shadow'));
     heatherForest_div.classList.add('shadow'); 
     heatherForest_div.scrollIntoViewIfNeeded();
 });
+cabernetHighTopDD_div.addEventListener("click", function (e) {
+    dropDown.forEach(shoeimage=> shoeimage.classList.remove('shadow'));
+    cabernetHighTop_div.classList.add('shadow'); 
+    cabernetHighTop_div.scrollIntoViewIfNeeded();
+});
+springMossDD_div.addEventListener("click", function (e) {
+    dropDown.forEach(shoeimage=> shoeimage.classList.remove('shadow'));
+    springMoss_div.classList.add('shadow'); 
+    springMoss_div.scrollIntoViewIfNeeded();
+});
+springMossDD_div.addEventListener("click", function (e) {
+    dropDown.forEach(shoeimage=> shoeimage.classList.remove('shadow'));
+    springMoss_div.classList.add('shadow'); 
+    springMoss_div.scrollIntoViewIfNeeded();
+});
+
 
 
 
--- app_ideas.js
+++ app_ideas.js
@@ -2,11 +2,6 @@
 designer and then click on menu item to direct you to place on page 
 that has section for each item. perhaps on click make image and video size bigger to 
 show which item is selected. 
-
-2) on hover play video links 
-    May be a good idea to store the video links as an array
-     and access them via index? 
-
 */ 
 
 /*let workingHard_span = document.getElementById('working-hard-message').innerHTML;
